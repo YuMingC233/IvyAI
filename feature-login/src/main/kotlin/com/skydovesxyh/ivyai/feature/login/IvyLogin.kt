38203a5d9f0b37257cd8fcd409380f885564302b
Connor White <YuMingC233@gmail.com>
2024-11-18 22:11:20 +0800

    rm: 删除原作者注释

--- feature-login/src/main/kotlin/com/skydovesxyh/ivyai/feature/login/IvyLogin.kt
+++ feature-login/src/main/kotlin/com/skydovesxyh/ivyai/feature/login/IvyLogin.kt
@@ -1,18 +1,3 @@
-/*
- * Designed and developed by 2024 skydoves (Jaewoong Eum)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.skydovesxyh.ivyai.feature.login
 
@@ -22,17 +7,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import com.skydovesxyh.ivyai.core.designsystem.theme.BACKGROUND900
 
 @Composable
+@Preview
 fun ChatGPTLogin(
 ) {
   Box(
     modifier = Modifier
       .fillMaxSize()
-      .background(BACKGROUND900)
-
+      .background(Color(0xFFffffff))
   ) {
-    Text(text = "我的页面")
+
   }
 }
